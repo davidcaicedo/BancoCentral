38b2d8b3b97f190e3ae2c8de4502d951f4f5a086
davidcaicedo <davidcaicedoq@gmail.com>
2019-07-18 18:01:15 -0500

    Commit 3 fin de clase

--- api/router.js
+++ api/router.js
@@ -1,25 +1,25 @@
 module.exports = (app) => {
 
     // Funciones que responden a los metodos http
-        app.get('/', function(req, res){
-            let persona = {
-                'nombre' : 'David',
-                'edad'   : '29'
-            };
-            res.send( persona );
-        });
-    
-        app.post('/personas', (req, res) =>{
-            let nombre = req.body.nombre;
-            let edad = req.body.edad;
-            let miPersona = {
-                'elNombre' : nombre,
-                'laEdad'   : nombre,
-            };
-    
-            res.send( miPersona );
-    
-        });
-    };
+    app.get('/', function(req, res){
+        let persona = {
+            'nombre' : 'David',
+            'edad'   : '29'
+        };
+        res.send( persona );
+    });
+
+    app.post('/personas', (req, res) =>{
+        let nombre = req.body.nombre;
+        let edad = req.body.edad;
+        let miPersona = {
+            'elNombre' : nombre,
+            'laEdad'   : nombre,
+        };
+
+        res.send( miPersona );
+
+    });
+};
     
     
---
+++ bd/bd.js
@@ -0,0 +1,36 @@
+const mysql = require('mysql');
+// Creamos la clase de conexion para este caso es MYSQL
+class DB{
+    constructor(){
+        if( !DB.instancia ){
+            DB.instancia = this;
+
+            // Agregamos los parámetros de conexión
+            this.connection = mysql.createConnection({
+                host     : '127.0.0.1',
+                user     : 'root',
+                password : '',
+                database : ''
+            });
+
+            // conectamos y manejamos la conexion con throw
+            this.connection.connect((err) => {
+                if(err){
+                    throw err;
+                }                    
+            });
+            console.log( 'Entró a conectar' );
+        }
+
+        // Si existe la instancia, que la retorne
+        console.log( 'Encontró una instancia de conexión.' );
+        return DB.instancia;
+    }
+}
+
+// Instanciamos la base de datos
+const instancia = new DB();
+// Freeze por seguridad, para que no se puedan modificar las variables
+Object.freeze( instancia );
+
+module.exports = instancia;
